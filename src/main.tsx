c14de70599b9a506343a727277259db689884a64
Daniil Zhadovsky <zhadovskii@tochka.com>
2026-07-12 23:00:29 +0400

    add PWA support

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,10 @@ import './styles.css';
 
 if(import.meta.env.DEV)validateWorkoutContent(workoutExercises);
 
+if('serviceWorker' in navigator&&import.meta.env.PROD){
+  window.addEventListener('load',()=>navigator.serviceWorker.register(`${import.meta.env.BASE_URL}sw.js`));
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
